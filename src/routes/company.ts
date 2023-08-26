7a480899764d3accd34b4ff8d23ca4f0ef02697e
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-08-26 02:22:24 -0700

    feat: add jwt company crud

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -4,30 +4,35 @@ import { RouteOptions } from "fastify";
 export const GetTest: RouteOptions = {
   method: "GET",
   url: "/api/company",
+  onRequest: (f) => f.jwtVerify(),
   handler: Controller.Company.Get,
   schema: Controller.Company.Schema.Get,
 };
 export const GetCompanyById: RouteOptions = {
   method: "GET",
   url: "/api/company/:id",
+  onRequest: (f) => f.jwtVerify(),
   handler: Controller.Company.GetById,
   schema: Controller.Company.Schema.GetById,
 };
 export const CreateCompany: RouteOptions = {
   method: "POST",
   url: "/api/company",
+  onRequest: (f) => f.jwtVerify(),
   handler: Controller.Company.Create,
   schema: Controller.Company.Schema.Create,
 };
 export const UpdateCompany: RouteOptions = {
   method: "PUT",
   url: "/api/company/:id",
+  onRequest: (f) => f.jwtVerify(),
   handler: Controller.Company.Update,
   schema: Controller.Company.Schema.Update,
 };
 export const DeleteCompany: RouteOptions = {
   method: "DELETE",
   url: "/api/company/:id",
+  onRequest: (f) => f.jwtVerify(),
   handler: Controller.Company.Delete,
   schema: Controller.Company.Schema.Delete,
 };
