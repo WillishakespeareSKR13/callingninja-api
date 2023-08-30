7686dce4175d4095f81648b6c9309fb08bc5850b
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-08-30 16:11:56 -0700

    fix paths

--- src/controllers/audios/index.ts
+++ src/controllers/audios/index.ts
@@ -61,7 +61,7 @@ export const SignedUrl: IController = async (req) => {
 
   const urlName = `${audioCreate.id}.${ext}`;
 
-  const url = `https://storage.googleapis.com/${CONFIG.GOOGLE.BUCKET}/${CONFIG.APP.NAME}/audios${companyId}/${urlName}`;
+  const url = `https://storage.googleapis.com/${CONFIG.GOOGLE.BUCKET}/${CONFIG.APP.NAME}/audios/${companyId}/${urlName}`;
 
   const [signedUrl] = await storage
     .bucket(CONFIG.GOOGLE.BUCKET)
